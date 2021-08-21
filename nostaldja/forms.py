45866ce6de2c6d86394d8bfb53597af2e2044716
nerofmt <trevor4n@gmail.com>
2021-08-21 16:09:40 -0500

    initial development of UI

---
+++ nostaldja/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import fields
+from .models import Decade, Fad
+
+class DecadeForm(forms.ModelForm):
+    class Meta:
+        model = Decade
+        fields = ('start_year')
+
+class FadForm(forms.ModelForm):
+    class Meta:
+        model: Fad
+        fields = (
+            'name',
+            'image_url',
+            'description'
+        )
