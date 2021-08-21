8f500cd932196a1e7f0e019d755c126dc52b4939
nerofmt <trevor4n@gmail.com>
2021-08-21 13:42:03 -0500

    created & migrated Decade % Fad models

--- nostaldja/models.py
+++ nostaldja/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Decade(models.Model):
+    start_year = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.start_year
+
+class Fad(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField(default='blank')
+    description = models.CharField(max_length = 100)
+    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+
+    def __str__(self):
+        return self.name
